26a61a1c6e643dd0ac276573b970b709eca0df37
Abdollah Mohammadi <abdollah.mohammadi@cern.ch>
2026-05-01 12:15:02 -0500

    debug

--- plugins/condor_analyzer.sh
+++ plugins/condor_analyzer.sh
@@ -68,10 +68,13 @@ done
 
 
 
-hadd hzz4l_out_${name}_${process}.root  *_outX.root
-# eval `scram unsetenv -sh`
+hadd out_${name}_${process}.root  *_outX.root
+eval `scram unsetenv -sh`
 
-gfal-copy -p hzz4l_out_${name}_${process}.root   davs://cmsxrootd.hep.wisc.edu:1094/store/user/abdollah/hzz4l_out_${name}_${process}.root
+gfal-copy -p test.root   davs://cmsxrootd.hep.wisc.edu:1094/store/user/abdollah/test.root
+gfal-copy -p test_${process}.root   davs://cmsxrootd.hep.wisc.edu:1094/store/user/abdollah/test_${process}.root
+
+gfal-copy -p out_${name}_${process}.root   davs://cmsxrootd.hep.wisc.edu:1094/store/user/abdollah/out_${name}_${process}.root
 #gfal-copy -p X_Mini.root   davs://cmsxrootd.hep.wisc.edu:1094/store/user/abdollah/OUTROOT_${name}_${process}_Mini.root
 
 
